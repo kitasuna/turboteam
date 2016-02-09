cc589265a5dbbb0fb08759cf078dce621e17acd7
eric <eric@bandwidthproductions.com>
2016-02-09 19:03:09 +0900

    break functions into another file

---
+++ text_functions.py
@@ -0,0 +1,59 @@
+import re
+from collections import namedtuple
+
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+def is_line_date(line):
+    pattern = re.compile('^(\d+/\d+/\d+)')
+    return pattern.match(line) 
+
+def is_line_chat(line):
+    pattern = re.compile('^(\d+:\d+\t(.+)\t(.+))')
+    return pattern.match(line) 
+
+def parse_line(line, current_date = False):
+    Dateline = namedtuple('Dateline', 'date')
+    Chatline = namedtuple('Chatline', 'user text')
+    Badline = namedtuple('Badline', 'text')
+
+    l = Badline(line)
+
+    match = is_line_date(line)
+    if match:
+        l = Dateline(match.group(1)) 
+
+    match = is_line_chat(line)
+    if match:
+        l = Chatline(match.group(2), match.group(3)) 
+
+    return l
+
+# Takes in a line and a dictionary as follows
+# {person's name, {word, word frequency}}
+def line_word_count(line, person, word_dict):
+    words = line.split()
+    for word in words:
+        word = word.lower() #makes everything lowercase
+        word_dict[person].setdefault(word, 0)
+        word_dict[person][word] += 1
+
+    return word_dict
+
+# Pretty self explanatory if I do say so myself
+def he_says_what_how_much(he, what, where):
+    print( str(he) + ' says "' + str(what) + '" ' + str(where[he].get(what, 'actually, no he dont' )))
+
+# Really it is word density but fuck density sounds infinitely cooler
+def fuck_density(word, person, word_dict):
+    total_value = 0
+    for value in word_dict[person].values():
+        total_value += value
+    try:
+        return word_dict[person][word] / total_value
+    except KeyError:
+        return 0
+
--- tt.py
+++ tt.py
@@ -1,266 +1,71 @@
-import re
-
-from collections import namedtuple
-
+from text_functions import *
 from sys import argv
 
-
-
-def file_len(fname):
-
-    with open(fname) as f:
-
-        for i, l in enumerate(f):
-
-            pass
-
-    return i + 1
-
-
-
-def is_line_date(line):
-
-    pattern = re.compile('^(\d+/\d+/\d+)')
-
-    return pattern.match(line) 
-
-
-
-def is_line_chat(line):
-
-    pattern = re.compile('^(\d+:\d+\t(.+)\t(.+))')
-
-    return pattern.match(line) 
-
-
-
-def parse_line(line, current_date = False):
-
-    Dateline = namedtuple('Dateline', 'date')
-
-    Chatline = namedtuple('Chatline', 'user text')
-
-    Badline = namedtuple('Badline', 'text')
-
-
-
-    l = Badline(line)
-
-
-
-    match = is_line_date(line)
-
-    if match:
-
-        l = Dateline(match.group(1)) 
-
-
-
-    match = is_line_chat(line)
-
-    if match:
-
-        l = Chatline(match.group(2), 'text in here') 
-
-
-
-    return l
-
-        
-
-    
-
-# Takes in a line and a dictionary as follows
-
-# {person's name, {word, word frequency}}
-
-def line_word_count(line, person, word_dict):
-
-    words = line.split()
-
-    for word in words:
-
-        word = word.lower() #makes everything lowercase
-
-        word_dict[person].setdefault(word, 0)
-
-        word_dict[person][word] += 1
-
-
-
-    return word_dict
-
-
-
-# Pretty self explanatory if I do say so myself
-
-def he_says_what_how_much(he, what, where):
-
-    print( str(he) + ' says "' + str(what) + '" ' + str(where[he].get(what, 'actually, no he dont' )))
-
-
-
-# Really it is word density but fuck density sounds infinitely cooler
-
-def fuck_density(word, person, word_dict):
-
-    total_value = 0
-
-    for value in word_dict[person].values():
-
-        total_value += value
-
-    try:
-
-        return word_dict[person][word] / total_value
-
-    except KeyError:
-
-        return 0
-
-
-
 def main():
-
     script, filename = argv
-
     current_date = False
-
     daily_tally = 0;
-
     unused_lines = 0;
-
     daily_lines = {}
-
     dude_lines = {}
-
     word_count = {}
-
     with open(filename, 'r') as f:
-
         for line in f:
 
-
-
-            # Don't even bother if the line is all whitespace
-
-            if not line.strip():
-
-                unused_lines += 1
-
-                continue
-
-
-
+            # Figure out of this is a date or a chat entry
             l = parse_line(line)
 
-            print(l)
-
-            """
-
-            if is_line_date(line):
-
+            if type(l).__name__ == 'Dateline':
+                # If current date is already set, reset the tally
+                # and set a new date
                 if current_date:
-
                     daily_lines[current_date] = daily_tally
-
-                current_date = match.group(1)
-
-                daily_tally = 0
-
-            else:
-
+                    current_date = l.date
+                    daily_tally = 0
+                else:
+                    current_date = l.date
+            elif type(l).__name__ == 'Chatline':
                 daily_tally += 1 
 
-
-
                 # Find who wrote this one, if anyone
-
-                dude_match = dude_pattern.match(line)
-
-                if dude_match:
-
-                    dude_name = dude_match.group(2);
-
-                        #Austin fucking around here
-
-                        #Word count is a dictionary that counts words for each person
-
-                    word_count.setdefault(dude_name,{})
-
-                    line_word_count(line, dude_name, word_count)
-
-                    if dude_name in dude_lines:
-
-                        dude_lines[dude_name] += 1
-
-                    else:
-
-                        dude_lines[dude_name] = 1
-
+                dude_name = l.user
+                    #Austin fucking around here
+                    #Word count is a dictionary that counts words for each person
+                word_count.setdefault(dude_name,{})
+                line_word_count(l.text, l.user, word_count)
+                if dude_name in dude_lines:
+                    dude_lines[dude_name] += 1
                 else:
+                    dude_lines[dude_name] = 1
+            else:
+                print('No match for "', line, '"');
+                unused_lines += 1
 
-                    print('No match for', line);
-
-                    unused_lines += 1
-
-
-
-
-
-
-
-            # Write out the last day
-
-            if current_date:
-
-                daily_lines[current_date] = daily_tally
-
+        # Write out the last day
+        if current_date:
+            daily_lines[current_date] = daily_tally
         
-
         print('Line count is ' + str(file_len(filename)))
-
         print('sum of daily line count is', str(sum(daily_lines.values())))
-
         print('sum of dude line count is', str(sum(dude_lines.values())))
-
         print('unused line count is', unused_lines)
-
         print(daily_lines)
-
         print(dude_lines)
 
-
         
-
-        #Fuck density!
-
+        # Fuck density!
         for dude in word_count:
-
             print(dude + "'s fuck density is: " + str(fuck_density('fuck',dude,word_count)))
-
-        #Lists dudes to make console input easy
-
+        # Lists dudes to make console input easy
         for dude in word_count:
-
             print(dude)
 
-
-
         while input('Stay ') != 'no':
-
                 he_says_what_how_much(input('Who? '),input('What? (lowercase) '),word_count)
-
 #Commented code prints everything. Ridiculously long and not useful at all.
-
 #for people in word_count:
-
 #           print('***********************' + str(people) + ' says')
-
 #           print(word_count[people].items())
-
-
-        """
 
 main()
 
